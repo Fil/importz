e9e84ea67fe44fea6d69694bc8fc7890dd5637f1
Philippe Rivière <fil@rezo.net>
2024-02-02 14:10:27 +0100

    add require for bun

--- index.mjs
+++ index.mjs
@@ -1,5 +1,12 @@
-import foobar0 from "./foo bar.js";
-import foobar1 from "./foo%20bar.js";
-import foobar2 from "./foo%2520bar.js";
+import import0 from "./foo bar.js";
+import import1 from "./foo%20bar.js";
+import import2 from "./foo%2520bar.js";
 
-console.log({ foobar0, foobar1, foobar2 });
+console.log({ import0, import1, import2 });
+
+if (process?.title==="bun") {
+  const require0 = require( "./foo bar.js").default;
+  const require1 = require("./foo%20bar.js").default;
+  const require2 = require("./foo%2520bar.js").default;
+  console.log({ require0, require1, require2 });
+}
